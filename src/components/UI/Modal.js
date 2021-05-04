fa0b4716423826ce882a41447d1809c368ab9e2f
Chris McColgan <mccolganchrisj@gmail.com>
2021-05-04 13:27:17 -0500

    Added cart context

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -13,6 +14,21 @@ const ModalOverlay = (props) => {
   );
 };
 
-const Modal = (props) => {};
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
 
 export default Modal;
